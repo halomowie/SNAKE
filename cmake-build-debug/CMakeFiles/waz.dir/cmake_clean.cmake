23cf7edf75d28e133973a9042526e3d35fd396cf
halomowie <50609253+halomowie@users.noreply.github.com>
2019-06-07 14:17:08 +0200

    snake follow

---
+++ cmake-build-debug/CMakeFiles/waz.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/waz.dir/main.cpp.obj"
+  "CMakeFiles/waz.dir/snake.cpp.obj"
+  "CMakeFiles/waz.dir/fruit.cpp.obj"
+  "waz.pdb"
+  "waz.exe"
+  "waz.exe.manifest"
+  "libwaz.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/waz.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
